7f366f58796856fac48d3781760630fffb4c359a
JhonatanGAlves <jhonatangalves96@gmail.com>
2022-01-25 20:23:55 -0300

    applying styles and importing components

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,28 @@
 import React from 'react'
-import './App.css'
+import styled from 'styled-components'
+import imgBKG from './assets/images/background.jpg'
+import Modal from './components/Modal'
 
 export const App = () => {
   return (
-    <div className="App">
-      Testando
-    </div>
+    <StyledApp className="App">
+      <Modal />
+    </StyledApp>
   )
 }
+
+const StyledApp = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+
+  // -----BKG CSS-----
+  background-image: url(${imgBKG});
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  position: relative;
+`
 
 export default App
